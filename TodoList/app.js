cd3e301320233a1e1cc44293363f8d9d4a3c0be0
Henryrivardo07 <henrysihombing20@gmail.com>
2024-04-04 12:06:31 +0700

    Tugas Pokemon

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,57 +1,51 @@
-//KUMPULKAN SEMUA UI ELEMENT( Coolection UI ELEMENT WITH DOM API)
-//NGAMBIL ELEMENT DI HTML BUAT DI PROSES LINE NO 3
-const todoForm = document.querySelector("#todo-form"); //ngambil id todo-form
+//KUMPULKAN SEMUA UI ELEMENT
+
+const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const filterInput = document.querySelector("#filter-input");
-const todolist = document.querySelector("#todo-list");
+const todoList = document.querySelector("#todo-list");
 const clearButton = document.querySelector("#clear-todos");
 
-//INI ADALAH KUMPULAN EVENT LISTENER
-//EVENT addEventListener ADALAH SEBUAH DOM API, YANG DAPAT MEMBUAT FUNSIONALITAS DALAM SEBUAH ELEMENT, YANG MEMBUAT WEB LEBIH INTERAKTIF
+//Eventlistener, sebuah Api Dom yang dapat memberikan sebuah fungsionalitas agar element itu berfungsi, semisal, click submit dll
+//p = pencet
+//INI ADALAH EVENT LISTENER
 
-immediateLoadEvenetListener();
-function immediateLoadEvenetListener() {
-  //MENDAPATKAN TODOS DARI LOCALSTORAGE DAN RENDER DI BROWSER
-  document.addEventListener("DOMContentLoaded", getTodos); //KETIKA DI REFRESH AKAN MENJALANKAN SEBUAH FUNCTION
+immediateLoadEventListener();
+function immediateLoadEventListener() {
+  document.addEventListener("DOMContentLoaded", getTodos);
 
-  //INI ADALAH EVENT UNTUK MENAMBAHKAN TODO
+  //Artinya todoForm ketika ada sebuah submit, akan menjalankan function addTodo
   todoForm.addEventListener("submit", addTodo);
-  //INI ADALAH EVENT UNTUK MENGHAPUS TODO
-  todolist.addEventListener("click", deleteTodo);
-  //INI ADALAH EVENT UNTUK MENGHAPUS SEMUA TODO
-  clearButton.addEventListener("click", deleteAllTodo);
-  //INI ADALAH EVENT UNTUK MENFILTER TODO
-  filterInput.addEventListener("keyup", filterLists);
+  //Artinya ketika Delete di clik menjalankan fungsi deleteButton
+  todoList.addEventListener("click", deleteButton);
+  //Artinya ketika DELETE DI CLIK AKAN MENJALANKAN CLEAR ALL BUTTON
+  clearButton.addEventListener("click", clearAll);
+  //ARTINYA MENCARI TEXT FILTER
+  filterInput.addEventListener("keyup", filter);
 }
 
 //REUSABLE CODE
+
 function createTodoElement(value) {
-  //MEMBUAT LI ELEMENT, dari API DOM, YAITU MEMBUAT LI DI HTML TANPA HARDCODE,YAITU LEWAT JAVASCRIPT
-  const li = document.createElement("li"); // li ELEMENT BERBENTUK OBJEK
-  //MENAMBAHKAN CLASS PADA ELEMENT LI
+  const li = document.createElement("li");
+  //2.membuat properti class, dalam li element
   li.className = "list-group-item d-flex justify-content-between align-items-center mb-1 todo-item";
-  //MENAMBAHKAN CHILDREN KE DALAM ELEMENT LI
+  //3.membuat children kedalam element li = li.appendChild
   li.appendChild(document.createTextNode(value));
-  //USER MASUKIN TEXT SENDIRI
-  //KENAPA todoInput.value karena OBJEK KALO GA ADA PROPERTINYA BUAT OBJEK BARU
-
-  //MEMBUAT DELETE BUTTON
+  //4.membuat children element a kedalam element li
   const a = document.createElement("a");
-
-  //MEMBERI PROPERTI UNTUK a ELEMENT
+  //5.memberi properti untuk element a, dalam DOM ,properti akan menjadi objek
   a.href = "#";
-  a.className = "badge badge-danger delete-todo";
-
-  //MEMASUKAN CHILD BISA PALE appendChild atau innerHTML, BEDANYA KALO append dimasuk ke sebelahnnya kalo inner html mengganti semua
+  a.className = "badge badge-danger delete-button";
   a.innerHTML = "Delete";
-
-  //MENYELIPKAN ELEMENT A KEDALAM ELEMENT CHILDREN LI
+  //6. menyatukan element li dan element a
   li.appendChild(a);
-  //MEMASUKAN ELEMENT LI YANG TELAH DIBUAT DENGAN JAVASCRIPT KE DALAM ELEMET TODOLIST
-  todolist.appendChild(li); //GA PAKE INNER KARENA BIAR GA HAPUS "VALUE DARI TASK INPUT"
+
+  //8.Memasukan element li yang telah dibuat dari javaScript kedalam element HTML todoList
+  todoList.appendChild(li);
 }
 
-function getItemFromLocalStorage() {
+function getFromLocalStorage() {
   let todos;
 
   if (localStorage.getItem("todos") === null) {
@@ -59,106 +53,119 @@ function getItemFromLocalStorage() {
   } else {
     todos = JSON.parse(localStorage.getItem("todos"));
   }
-
   return todos;
 }
 
-//JAVASCRIPT HOISTING
-
 //INI ADALAH DOM FUNCTION
-
+//MENDAPATAKAN TODOS DARI LOCAL STORAGE DAN TAMPILKAN
 function getTodos() {
-  const todos = getItemFromLocalStorage();
-  todos.forEach((todo) => {
-    createTodoElement(todo);
+  const todos = getFromLocalStorage();
+  //KARENA DI LOCAL STORAGE BENTUKAN ARRAY KITA BISA MUNCULIN 1 1 PAKE LOOPING FOR EACH
+
+  todos.forEach((item) => {
+    createTodoElement(item);
   });
-  //program menjalankan forEach pada array todos, setiap elemen dalam array tersebut akan diteruskan sebagai argumen ke fungsi callback createTodoElement. Dengan demikian, hasil dari createTodoElement
 }
+//7. Membuat apa yang di tulis secara dynamic data (user yang ketik), dengan todoInput di virtual DOM, muncul di function todoList
+//- Sebuah input yang di ketik akan menghasilkan properti, value(properti = objek), jadi di line li.appenChild(todoInput.value) artinya isi parameternya nilai objek dari todoInput.value, value sebagai key
 
-function addTodo(e) {
-  e.preventDefault(); //preventDefault BEHAVIOR AGAR TIDAK REFRESH PAGE, KALO PAGE DI INPUT
+//9.mendelete parent element dengan p yang artinya pencet
+function addTodo(p) {
+  //12.Menambahkan logic agar user tidak meninput value kosong
+  p.preventDefault();
   if (todoInput.value) {
-    //KONDISI KALO ADA VALUE
-    createTodoElement(todoInput.value); //HASIL INPUTAN DI BROWSER, AKAN DI JALANKAN DI FUNCTION createTodoElement()
-
+    // membuat virtual DOM, seperti halnya hardcode di html
+    //1.Buat li element = document.createElement
+    createTodoElement(todoInput.value);
+    //INPUT YANG DIKETIK AKAN MASUK KE  VALUE LALU DIKIRIM KE CREATE TODO ELEMENT SETELAH ITU DI PROSES DAN DIMASUKAN LAGI KE DALAM FUNCTION
     addTodoLocalStorage(todoInput.value);
-    todoInput.value = "";
+    todoInput.value = " ";
+    //12.mereset ulang value dari objek todoInput dengan key value di reset
 
-    /*todoInput.value = "", Anda mengatur nilai properti value dari elemen todoInput menjadi string kosong, sehingga mengosongkan isian input
-     */
-    //MENRECREATE HTML DENGAN VANILLA JAVASCRIPT(DOM API)
+    //proses dari awal sampai akhir dimasukan di local storage, ini hoisting doang di pass ke function addToLocalStorage yang bawah
   } else {
-    alert("BELOM MASUKAN TEXT");
-  }
+    alert("Harap masukan isi");
+  } //17 MEMBUAT SEMUA TASK YANG DIBUAT DI SIMPEN DI LOCAL STORAGE
 }
-
+// 18 menambahkan data yang telah dimasukan dalam function addTodo kedalam local data storage
 function addTodoLocalStorage(todoInputValue) {
-  const todos = getItemFromLocalStorage();
+  // 19. membuat variabel yang menampung data nya dulu
+  const todos = getFromLocalStorage();
+  //KALO MAU TARO DI LOCAL STORAGE PAKE STRING
+
   todos.push(todoInputValue);
 
-  localStorage.setItem("todos", JSON.stringify(todos)); //DIDALAM LOCAL STORAGE VALUE HARUS BERUPA STRING
+  localStorage.setItem("todos", JSON.stringify(todos));
+  /*
+  Pada langkah nomor 3, ketika terdapat nilai yang tersimpan di local storage, nilai tersebut diambil menggunakan localStorage.getItem("todos"), kemudian dikonversi dari format JSON menjadi bentuk array JavaScript menggunakan JSON.parse(). Hal ini dilakukan agar data yang disimpan dalam local storage (yang awalnya disimpan dalam bentuk string JSON) dapat diolah sebagai array JavaScript.
+
+Setelah nilai tersebut diubah ke dalam bentuk array JavaScript, kemudian nilai dari array tersebut dimasukkan ke dalam variabel todos menggunakan metode push(), sehingga data baru dapat ditambahkan ke dalam data yang sudah ada sebelumnya.
+
+Setelah proses penambahan data selesai dilakukan, variabel todos yang sudah diperbarui akan dikonversi kembali ke dalam format JSON menggunakan JSON.stringify(todos). Ini dilakukan agar nilai todos dapat disimpan kembali ke dalam local storage sebagai string JSON menggunakan localStorage.setItem("todos", JSON.stringify(todos)). Dengan demikian, data yang sudah diperbarui akan tetap tersimpan di local storage.
+*/
 }
 
-function deleteTodo(e) {
-  e.preventDefault();
+function deleteButton(p) {
+  p.preventDefault();
 
-  if (e.target.classList.contains("delete-todo")) {
-    if (confirm("APAKAH YAKIN AKAN MENGHAPUS?")) {
-      //KOMFIRMASI USER
-
-      const parent = e.target.parentElement;
+  if (p.target.classList.contains("delete-button")) {
+    //10.jika p pencet button dengan mengandung delete-button
+    if (confirm("Apakah yakin akan menghapus?")) {
+      //11.Membuat confirm apakah ingin menghapus
+      const parent = p.target.parentElement;
 
       parent.remove();
 
-      deleteTodoLocalStorage(parent);
+      deleteFromLocalStorage(parent);
     }
-    //TAPI KEHAPUSNYA HANYA DI SEMENTARA, KARENA element li di tulis di hardcode HTML, DAN DIHAPUSNYA DI DOM
   }
 }
 
-function deleteTodoLocalStorage(deletedElement) {
-  let todos = getItemFromLocalStorage(); //MENGHAPUS ELEMENT PARENT 4 YAITU (li)
+function deleteFromLocalStorage(deletedElement) {
+  const todos = getFromLocalStorage(); //1,2,3,4,5
 
-  todos.forEach((todo, index) => {
+  // todos.forEach((todo, index) => {
+  //   if (deletedElement.firstChild.textContent === todo) {
+  //     todos.splice(index, 1);
+  //   }
+  // });
+
+  todos.forEach(myFunction);
+
+  function myFunction(todo, index) {
+    //FOR EACH DEFAULT NGASIH 2 PARAMETER(VALUE , INDEX)
     if (deletedElement.firstChild.textContent === todo) {
-      todos.splice(index, 1);
+      //contoh 5
+      todos.splice(index, 1); //4,1 yang dihapus
     }
-  });
+  }
 
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function deleteAllTodo() {
-  //YANG INI GA BUTUH ELEMENT KARENA LANGSUNG SATU BLOK DI HAPUS
-  if (confirm("ALL CLEAR?")) {
-    todolist.innerHTML = "";
-  }
-  clearTodosLocalStorage();
+function clearAll() {
+  todoList.innerHTML = "";
+  clearAllTodo();
 }
 
-// function clearTodos() {
-//   todolist.innerHTML = "";
-//
-// }
-
-function clearTodosLocalStorage() {
+function clearAllTodo() {
   localStorage.clear();
 }
 
-function filterLists(e) {
-  const filterText = e.target.value.toLowerCase(); //KETIKAN PENCARI
+//KALO VALUE DARI OBJEK FILTERINPUT.VALUE DIBANDINGKAN DENGAN LIST
+function filter(p) {
+  const filterText = filterInput.value.toLowerCase();
+  //BISA JUGA PAKE p.target
   const todoItems = document.querySelectorAll(".todo-item");
+  //14. KAYA VARIABEL ISINYA ARRAY, BARU NANTI DI LOOPING AGAR BISA DI CEK 1 1
 
   todoItems.forEach((item) => {
-    // INI DAPATNYA DARI PENGAMBILAN LINE 79 DIMANA CLASS NYA DI AMBIL PAKE querySelectorALL
     const itemText = item.firstChild.textContent.toLowerCase();
+    //LOPPINGNYA DISINI DI CEK PER INDEX, DAN DIMASUKAN KE VAR BARU
     if (itemText.indexOf(filterText) !== -1) {
-      // APAKAH YANG DIKETIK DILINE 78 ADA GA DI LINE 82
       item.setAttribute("style", "display:block");
     } else {
-      item.setAttribute("style", "display:none !important");
+      item.setAttribute("style", "display: none !important");
     }
-    console.log(itemText);
   });
-  // console.log(todoItems);
 }
